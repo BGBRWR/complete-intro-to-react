005dedb6f7516f4316a8a39d5b9ee1bb7f53c80a
Austin Walker <bgbrwr@gmail.com>
2016-12-08 14:51:02 -0700

    setup webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,15 +1,7 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1({style: {color: this.props.color}}, this.props.title)
-      )
-    )
-  }
-})
 
 var MyTitleFact = React.createFactory(MyTitle)
 var ce = React.createElement
@@ -17,8 +9,7 @@ var ce = React.createElement
 var MyFirstComponent = (
   div(null,
     MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
-    React.createElement(MyTitle, {title: 'Use props everywhere!', color:
-  'mediumaquamarine'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'}),
     ce(MyTitle, {title: 'Props are the best!', color: 'peru'})
   )
 )
